7f8e9923fb00ce437fd87cad7d722048eda13aba
Hayden Huggins <30280091+Hayden-Huggins@users.noreply.github.com>
2020-02-03 10:08:09 +1000

    First commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FetchRandomUser from './components/FetchRandomUser';
+
+ReactDOM.render(<FetchRandomUser />, document.getElementById('root'));
